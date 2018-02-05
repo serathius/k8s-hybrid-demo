69f9b881fdd6f6edc5dfcb0af7b030209f92c58e
Marek Siarkowicz <siarkowicz@google.com>
2018-02-05 10:12:59 +0100

    Implement breaking single node.

--- Makefile
+++ Makefile
@@ -9,6 +9,12 @@ deploy:
 break-overload-frontend:
 	LOAD_USER_COUNT=100 LOAD_REPLICAS=10 envsubst < manifests/load-deployment.json | kubectl apply -f -
 
+break-node:
+	./scripts/break-node.sh
+
+break-node-revert:
+	./scripts/break-node.sh REVERT
+
 clean:
 	find manifests/ -type f | xargs cat | envsubst | kubectl delete -f -
 
---
+++ scripts/break-node.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+: ${PROJECT_NAME?"missing environment variable PROJECT_NAME"}
+NODE_HOST=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}' | cut -f1 -d' ')
+MASTER_ENDPOINT=$(gcloud container clusters describe ${PROJECT_NAME} | grep endpoint | cut -f2 -d' ')
+if [ "$1" == "REVERT" ]; then
+  echo "Reverting breaking node: ${NODE_HOST}"
+  gcloud compute ssh ${NODE_HOST} -- "while \$(sudo iptables --delete OUTPUT --destination ${MASTER_ENDPOINT} --jump REJECT); do :; done"
+  EXPECTED_READY_STATE="True"
+else
+  echo "Fixing node: ${NODE_HOST}"
+  gcloud compute ssh ${NODE_HOST} -- sudo iptables --insert OUTPUT --destination ${MASTER_ENDPOINT} --jump REJECT
+  EXPECTED_READY_STATE="Unknown"
+fi
+NODE_CURRENT_READY_STATE=$(kubectl get node ${NODE_HOST} -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
+while [ ${NODE_CURRENT_READY_STATE} != ${EXPECTED_READY_STATE} ] ; do
+  echo "Waiting for node state READY=${EXPECTED_READY_STATE}, current state READY=${NODE_CURRENT_READY_STATE}"
+  sleep 3
+  NODE_CURRENT_READY_STATE=$(kubectl get node ${NODE_HOST} -o jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
+done;
+echo "Node status: ${NODE_CURRENT_READY_STATE}"
