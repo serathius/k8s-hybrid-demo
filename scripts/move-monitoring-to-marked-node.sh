f576a9d889b4c45ee52a3620105410d7abe52f90
Marek Siarkowicz <siarkowicz@google.com>
2018-02-14 11:02:05 +0100

    Fix move monitoring to selected node script.

--- scripts/move-monitoring-to-marked-node.sh
+++ scripts/move-monitoring-to-marked-node.sh
@@ -6,13 +6,12 @@ function patch {
   JSON_PATH=$3
   VALUE=$4
   PATCH=$5
-  if [ $(kubectl -n monitoring get ${TYPE} ${NAME} -o jsonpath="${JSON_PATH}") != "${VALUE}" ] ; then
-    kubectl -n monitoring patch prometheus k8s --patch "$PATH" --type=merge
+  if [ "$(kubectl -n monitoring get ${TYPE} ${NAME} -o jsonpath=${JSON_PATH})" != "${VALUE}" ] ; then
+    kubectl -n monitoring patch ${TYPE} ${NAME} --patch "$PATCH" --type=merge
   fi
 }
 patch prometheus k8s '{.spec.nodeSelector.purpose}' 'monitoring' '{"spec":{"nodeSelector":{"purpose":"monitoring"}}}'
 patch alertmanager main '{.spec.nodeSelector.purpose}' 'monitoring' '{"spec":{"nodeSelector":{"purpose":"monitoring"}}}'
-
 patch deployment prometheus-operator '{.spec.template.spec.nodeSelector.purpose}' 'monitoring' '{"spec":{"template":{"spec":{"nodeSelector":{"purpose":"monitoring"}}}}}'
 patch deployment grafana '{.spec.template.spec.nodeSelector.purpose}' 'monitoring' '{"spec":{"template":{"spec":{"nodeSelector":{"purpose":"monitoring"}}}}}'
 patch deployment kube-state-metrics '{.spec.template.spec.nodeSelector.purpose}' 'monitoring' '{"spec":{"template":{"spec":{"nodeSelector":{"purpose":"monitoring"}}}}}'
