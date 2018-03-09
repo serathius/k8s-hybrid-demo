9e2a7ef1a450a17e5c4c836276c343132bf01bf4
Marek Siarkowicz <siarkowicz@google.com>
2018-03-09 12:29:37 +0100

    Add nginx ingress

--- scripts/break-conn-node-lb.sh
+++ scripts/break-conn-node-lb.sh
@@ -3,6 +3,7 @@ NODES=($(kubectl get nodes -l 'purpose notin (monitoring)' | tail -n +2 | grep '
 INDEX=$(($RANDOM % ${#NODES[@]}))
 NODE=${NODES[$INDEX]}
 echo "Breaking communication to lb on node ${NODE}"
-LB_POD_IP=$(kubectl get pods -l run=haproxy-ingress -o jsonpath='{.items[0].status.podIP}')
+HAPROXY_POD_IP=$(kubectl get pods -l run=haproxy-ingress -o jsonpath='{.items[0].status.podIP}')
+NGINX_POD_IP=$(kubectl get pods -l app=ingress-nginx -o jsonpath='{.items[0].status.podIP}')
 
-gcloud compute ssh ${NODE} -- "while true; do sudo iptables --insert KUBE-FORWARD --source ${LB_POD_IP} --jump REJECT; sleep 1; done"
+gcloud compute ssh ${NODE} -- "while true; do sudo iptables --insert KUBE-FORWARD --source ${HAPROXY_POD_IP} --jump REJECT; sudo iptables --insert KUBE-FORWARD --source ${NGINX_POD_IP} --jump REJECT;sleep 1; done"
